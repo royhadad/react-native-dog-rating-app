26d1a612b703ebc866c5efb31f5de7e8b96a3f7c
Roy Hadad <roy@datree.io>
2024-03-26 22:40:54 +0800

    temp: unknown commit, might revert later

--- app/(tabs)/scoreboardScreen.tsx
+++ app/(tabs)/scoreboardScreen.tsx
@@ -1,4 +1,4 @@
-import { Button, StyleSheet } from "react-native";
+import { Button, Image, ScrollView, StyleSheet } from "react-native";
 
 import { Text, View } from "@/components/Themed";
 import { useTop10RatedDogs } from "@/external_clients/ratingService/ratingServiceQueries";
@@ -16,21 +16,41 @@ export default function ScoreboardScreen() {
         darkColor="rgba(255,255,255,0.1)"
       />
       <Text>This is the scoreboard!</Text>
-      <View>
+      <View style={{ flexGrow: 1 }}>
         {top10RatedDogsQuery.isPending ? (
           <Text>Loading...</Text>
         ) : top10RatedDogsQuery.isError ? (
           <Text>Error: {top10RatedDogsQuery.error.message}</Text>
         ) : (
-          <View>
+          <View style={{ width: "100%" }}>
             <Text>Top 10 rated dogs:</Text>
-            {top10RatedDogsQuery.data.map((dog) => (
-              <View key={dog.dog}>
-                <Text>{dog.dog}</Text>
-                <Text>{dog.averageRating}</Text>
-                <Text>{dog.numberOfRatings}</Text>
+            <ScrollView style={{ width: "100%" }}>
+              <View style={{ gap: 10, width: "100%" }}>
+                {top10RatedDogsQuery.data.map(
+                  ({ dog, numberOfRatings, averageRating }) => (
+                    <View
+                      key={dog}
+                      style={{
+                        flexDirection: "row",
+                        alignItems: "center",
+                        width: "100%",
+                        justifyContent: "flex-start",
+                        gap: 20,
+                      }}
+                    >
+                      <Image
+                        source={{ uri: dog }}
+                        style={{ width: 70, height: 70, borderRadius: 35 }}
+                      />
+                      <View>
+                        <Text>average: {averageRating}</Text>
+                        <Text>rated: {numberOfRatings} times</Text>
+                      </View>
+                    </View>
+                  ),
+                )}
               </View>
-            ))}
+            </ScrollView>
           </View>
         )}
       </View>
